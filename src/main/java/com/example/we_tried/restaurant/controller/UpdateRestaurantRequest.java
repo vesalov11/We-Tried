d7532c22c530d578a6f7baa1dec17b07eaf97ec0
INankova <ivana.nankova@gmial.com>
2025-04-13 11:10:39 +0300

    Images

--- src/main/java/com/example/we_tried/restaurant/controller/UpdateRestaurantRequest.java
+++ src/main/java/com/example/we_tried/restaurant/controller/UpdateRestaurantRequest.java
@@ -3,6 +3,7 @@ package com.example.we_tried.restaurant.controller;
 import com.example.we_tried.restaurant.model.RestaurantType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalTime;
@@ -10,20 +11,18 @@ import java.time.LocalTime;
 @Data
 public class UpdateRestaurantRequest {
 
-    @NotBlank(message = "Restaurant name cannot be empty!")
+    @Size(min = 3, max = 20, message = "Restaurant name must be between 3 and 20 symbols!")
     private String name;
 
-    @NotBlank(message = "Restaurant address cannot be empty!")
+    @Size(min = 3, max = 20, message = "Restaurant name must be between 10 and 50 symbols!")
     private String address;
 
-    @NotBlank(message = "Restaurant number cannot be empty!")
     private String phoneNumber;
 
     private LocalTime openingTime;
 
     private LocalTime closingTime;
 
-    @NotNull(message = "Restaurant type cannot be empty!")
     private RestaurantType restaurantType;
 
     private String restaurantPicture;
